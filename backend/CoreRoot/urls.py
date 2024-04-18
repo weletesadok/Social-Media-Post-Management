a86c7845b41f3e787fd5e09ff857ec99110ed490
weletesadok <weletesadok@gmail.com>
2024-04-18 14:31:56 +0300

    about to finish the api

--- backend/CoreRoot/urls.py
+++ backend/CoreRoot/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for CoreRoot project.
+"""CoreRoot URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(("core.routers", "core"), namespace="core-api")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
