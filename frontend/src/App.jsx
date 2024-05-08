e0ded58ba1be131c097cfb86e5fefac9ccb99826
None <none@localhost.localdomain>
2024-05-08 17:47:38 -0400

    frontend being done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,52 @@
-export default ()=>{
-  return "something that is wrong to say"
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Home from "./pages/Home";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import SinglePost from "./pages/SinglePost";
+import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
+
+function App() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/post/:postId/"
+        element={
+          <ProtectedRoute>
+            <SinglePost />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile/:profileId/"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile/:profileId/edit/"
+        element={
+          <ProtectedRoute>
+            <EditProfile />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/login/" element={<Login />} />
+      <Route path="/register/" element={<Registration />} />
+      <Route path="*" element={<div>Page not found</div>} />
+    </Routes>
+  );
 }
+
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
